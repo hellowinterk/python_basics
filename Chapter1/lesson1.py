b5c6693732223e23cc84147d05ccae04cbad58ee
Aleksandrov, Dmitry V <dmitry.v.aleksandrov@diadice.net>
2019-09-27 22:40:19 +0300

    added comments

--- Chapter1/lesson1.py
+++ Chapter1/lesson1.py
@@ -2,6 +2,9 @@
 if __name__ == '__main__':
     print('Ура!')
     print('Исследование распространённости языков.')
+
+    # the part of english speakers
     print(1121 / 7539)
+
+    # the quantity of people learned Japanese
     print((128.3 - 128.2) / 128.3)
-    
