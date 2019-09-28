05fbeefa6ec7a6ed797b3b84d2456a7bc876a28a
Aleksandrov, Dmitry V <dmitry.v.aleksandrov@diadice.net>
2019-09-28 20:42:34 +0300

    added speaking parts

--- Chapter1/lesson1.py
+++ Chapter1/lesson1.py
@@ -34,3 +34,25 @@ if __name__ == '__main__':
 
     print(russian_web_part2)
     print(web_popular2)
+
+
+    total_web = 10
+    total_speakers = 7539
+
+    chinese_speakers = 1107
+    chinese_web_part = 0.017
+
+    english_speakers = 1121
+    english_web_part = 0.539
+
+    russian_speakers = 264.3
+    russian_web_part = 0.061
+
+    chinese_speakers_part = chinese_speakers / total_speakers
+    print('Доля людей, говорящих на китайском:' , chinese_speakers_part)
+
+    english_speakers_part = english_speakers / total_speakers
+    print('Доля людей, говорящих на английском:' , english_speakers_part)
+
+    russian_speakers_part = russian_speakers / total_speakers
+    print('Доля людей, говорящих на русском:' , russian_speakers_part)
