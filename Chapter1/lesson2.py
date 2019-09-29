7b87a5497c25a93dcf16f38504c7de4d9024a368
Aleksandrov, Dmitry V <dmitry.v.aleksandrov@diadice.net>
2019-09-29 11:51:26 +0300

    added first lists

--- Chapter1/lesson2.py
+++ Chapter1/lesson2.py
@@ -1,2 +1,13 @@
 intro_text = 'Исследуем статистику различных эмодзи.'
 print(intro_text)
+
+instagram = [1.02, 1.69, 0.774, 7.31, 2.36]
+print(instagram)
+
+grinning_row = ['Ухмыляюсь', 'Grinning', 2.26, 1.02, 87.3]
+print(grinning_row)
+
+emojixpress = [2.26, 19.1, 25.6, 233.0, 15.2, 22.7, 64.6, 87.5, 6.81, 6.0]
+
+total = emojixpress[0] + emojixpress[1] + emojixpress[2] + emojixpress[3] + emojixpress[4]
+print('{:.2f}'.format(total))
