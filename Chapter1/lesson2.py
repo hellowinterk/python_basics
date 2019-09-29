82608d2812eac33c9df1bf9b00fa199326803c36
Aleksandrov, Dmitry V <dmitry.v.aleksandrov@diadice.net>
2019-09-29 12:38:55 +0300

    added the part of selected emojis

--- Chapter1/lesson2.py
+++ Chapter1/lesson2.py
@@ -47,3 +47,14 @@ total_hands += 1
 #emoji 'Shrugging'
 total_hands += 2
 print(total_hands)
+
+emojixpress = [ 2.26, 19.1, 25.6, 233.0, 15.2, 22.7, 64.6, 87.5, 6.81, 6.0, 4.72, 24.7, 21.7, 
+10.0, 118.0, 3.31, 23.1, 1.74, 4.5, 0.0333]
+
+emojixpress_total = 1720
+selected_total = 0
+for element in emojixpress:
+	selected_total += element
+
+selected_part = selected_total / emojixpress_total
+print('Доля выбранных эмодзи в Emojixpress: {:.1%}'.format(selected_part))
