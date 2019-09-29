94f8ea044ce17dd4808f5f833c8606b8224aad0a
Aleksandrov, Dmitry V <dmitry.v.aleksandrov@diadice.net>
2019-09-29 12:50:50 +0300

    added the information on twitter part of the selected emojis

--- Chapter1/lesson2.py
+++ Chapter1/lesson2.py
@@ -58,3 +58,13 @@ for element in emojixpress:
 
 selected_part = selected_total / emojixpress_total
 print('Доля выбранных эмодзи в Emojixpress: {:.1%}'.format(selected_part))
+
+twitter = [87.3, 150.0, 0.0, 2270.0, 264.0, 565.0, 834.0, 432.0, 0.0, 478.0,
+198.0, 654.0, 98.7, 445.0, 1080.0, 697.0, 227.0, 0.0, 150.0, 932.0]
+
+twitter_total = 24500
+selected_total_twitter = 0
+for element in twitter:
+	selected_total_twitter += element
+selected_part_twitter = selected_total_twitter / twitter_total
+print('Доля выбранных эмодзи в Твиттере:{:.1%}'.format(selected_part_twitter))
